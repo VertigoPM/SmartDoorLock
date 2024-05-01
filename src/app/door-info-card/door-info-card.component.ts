5bbbdf2ab2daf9b662150460da2ccae978cc8670
HomePcReza <rreza.rezaeian@gmail.com>
2024-05-01 18:26:49 +0330

    add door-info-component

---
+++ src/app/door-info-card/door-info-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-door-info-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './door-info-card.component.html',
+  styleUrl: './door-info-card.component.css'
+})
+export class DoorInfoCardComponent {
+
+}
