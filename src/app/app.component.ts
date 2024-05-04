cd2af72d37e4d57c876afc3eaa006980be0fa2d0
HomePcReza <rreza.rezaeian@gmail.com>
2024-05-04 19:19:12 +0330

    craete card for open and close the door

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DoorInfoCardComponent } from "./door-info-card/door-info-card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, DoorInfoCardComponent]
 })
 export class AppComponent {
   title = 'SmartDoorLock';
